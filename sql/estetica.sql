f139bf641df64891c0b38b287b6f8291a1b09759
DamianTC1903 <74327566+DamianTC1903@users.noreply.github.com>
2020-11-24 22:44:06 -0600

    24 nov

--- sql/estetica.sql
+++ sql/estetica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-11-2020 a las 02:35:13
+-- Tiempo de generación: 25-11-2020 a las 05:41:35
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.29
 
@@ -40,9 +40,70 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`id_cliente`, `nombre_cliente`, `direccion_cliente`, `localidad_cliente`, `telefono_cliente`) VALUES
-(1, 'Jose damian Tun chim', 'calle 14', 'Timucuy', '9994-23-45-43'),
-(5, 'damian', 'tc', 'mexico', '9994-67-78-89'),
-(6, 'prueba', 'prueba', 'prueba', '9991-23-23-21');
+(25, 'damian', 'tuns', 'chim', '9994-65-45-34');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(11) NOT NULL,
+  `nombre_producto` varchar(100) NOT NULL,
+  `descripcion_producto` varchar(200) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `id_promocion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_producto`, `nombre_producto`, `descripcion_producto`, `precio`, `id_promocion`) VALUES
+(1, 'Tinte', 'Tinte color, marca', 150, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `promociones`
+--
+
+CREATE TABLE `promociones` (
+  `id_promocion` int(11) NOT NULL,
+  `Descuento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `promociones`
+--
+
+INSERT INTO `promociones` (`id_promocion`, `Descuento`) VALUES
+(1, 10),
+(2, 20),
+(3, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedores`
+--
+
+CREATE TABLE `proveedores` (
+  `id_prov` int(11) NOT NULL,
+  `nombre_prov` varchar(100) NOT NULL,
+  `direccion_prov` varchar(100) NOT NULL,
+  `telefono_prov` int(11) NOT NULL,
+  `email_prov` varchar(100) NOT NULL,
+  `id_producto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `proveedores`
+--
+
+INSERT INTO `proveedores` (`id_prov`, `nombre_prov`, `direccion_prov`, `telefono_prov`, `email_prov`, `id_producto`) VALUES
+(1, 'nombre', 'direccion', 99999, 'gmail', 1);
 
 -- --------------------------------------------------------
 
@@ -67,6 +128,28 @@ INSERT INTO `roles` (`id_rol`, `nombre_rol`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id_servicios` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `tipo_servicio` varchar(100) NOT NULL,
+  `fecha_servicio` datetime NOT NULL,
+  `precio_servicio` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`id_servicios`, `id_cliente`, `tipo_servicio`, `fecha_servicio`, `precio_servicio`) VALUES
+(32, 25, 'wqjwh', '2020-11-23 17:31:00', '12'),
+(33, 25, 'corte 2', '2020-11-23 19:09:00', '120');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -83,8 +166,29 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `contraseña_usuario`, `id_rol`) VALUES
 (1, 'damian', 'admin', 1),
-(2233, 'jose', 'damian', 1),
-(2234, 'prueba', 'prueba', 2);
+(2241, 'js', 'js', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventaproductos`
+--
+
+CREATE TABLE `ventaproductos` (
+  `id_venta` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad_producto` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `ventaproductos`
+--
+
+INSERT INTO `ventaproductos` (`id_venta`, `id_producto`, `cantidad_producto`, `total`, `id_cliente`, `fecha`) VALUES
+(15, 1, 4, 540, 25, '2020-11-24 19:10:00');
 
 --
 -- Índices para tablas volcadas
@@ -97,10 +201,36 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id_cliente`);
 
 --
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_promocion` (`id_promocion`);
+
+--
+-- Indices de la tabla `promociones`
+--
+ALTER TABLE `promociones`
+  ADD PRIMARY KEY (`id_promocion`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`id_prov`);
+
+--
 -- Indices de la tabla `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id_servicios`),
+  ADD KEY `id_cliente` (`id_cliente`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -110,6 +240,14 @@ ALTER TABLE `usuarios`
   ADD KEY `id_rol` (`id_rol`);
 
 --
+-- Indices de la tabla `ventaproductos`
+--
+ALTER TABLE `ventaproductos`
+  ADD PRIMARY KEY (`id_venta`),
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -117,23 +255,66 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `promociones`
+--
+ALTER TABLE `promociones`
+  MODIFY `id_promocion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id_servicios` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2235;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2242;
+
+--
+-- AUTO_INCREMENT de la tabla `ventaproductos`
+--
+ALTER TABLE `ventaproductos`
+  MODIFY `id_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_promocion`) REFERENCES `promociones` (`id_promocion`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD CONSTRAINT `servicios_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ventaproductos`
+--
+ALTER TABLE `ventaproductos`
+  ADD CONSTRAINT `ventaproductos_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `ventaproductos_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
